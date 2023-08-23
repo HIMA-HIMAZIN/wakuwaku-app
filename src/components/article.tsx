2e8b8a76140223a9129d7395b2a0b1b418cff786
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-08-23 15:12:59 +0900

    init

---
+++ src/components/article.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+import styles from "@/styles/components/article/article.module.scss";
+
+export function DecidedButton() {
+  const router = useRouter();
+
+  return (
+    <>
+      
+    </>
+  );
+}
